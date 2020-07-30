258abb0dd17365554d969da89b9ba0c8ed59dc9b
patriciabonaldy <bonaldypatricia@gmail.com>
2020-07-30 19:13:32 -0300

    cambios

--- cmd/beers-cli/main.go
+++ cmd/beers-cli/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"runtime/pprof"
+
 	"github.com/patriciabonaldy/punkapi/internal/cli"
 	service "github.com/patriciabonaldy/punkapi/internal/cli/fetching"
 	storage "github.com/patriciabonaldy/punkapi/internal/cli/storage"
@@ -8,11 +11,21 @@ import (
 )
 
 func main() {
+	//CPU profiling code start here
+	f, _ := os.Create("beers.cpu.prof")
+	defer f.Close()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 
 	repo := storage.NewRepository()
 	fetch := service.NewFetchy(repo)
 	rootCmd := &cobra.Command{Use: "beers-cli"}
 	rootCmd.AddCommand(cli.InitBeersCmd(fetch))
 	rootCmd.Execute()
+
+	//Memory profiling code start here
+	f2, _ := os.Create("beers.mem.prof")
+	defer f2.Close()
+	pprof.WriteHeapProfile(f2)
 
 }
